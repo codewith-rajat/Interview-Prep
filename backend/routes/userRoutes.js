e6be85c9beb5a7e1823206c36469a565920862b1
codewith-rajat <rajatagrawal021@gmail.com>
2026-02-01 22:45:17 +0530

    created userRoutes

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();    
+const {protect} = require("../middlewares/authMiddleware");
+const { getProfile, updateProfile } = require("../controllers/userController");
+
+router.get("/profile",protect,(req,res)=>{
+    res.json(req.user);
+});
+
+router.get("/profile",protect,getProfile);
+router.put("/profile",protect,updateProfile);
+
+module.exports = router;
